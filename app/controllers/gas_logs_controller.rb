e53e7ed7c61a12d3e7295a46c09144e7815580a1
ecmisra <eshanmisra@gmail.com>
2019-02-06 12:14:07 -0500

    changing controller and model to eliminate requirement of total value

--- app/controllers/gas_logs_controller.rb
+++ app/controllers/gas_logs_controller.rb
@@ -52,7 +52,6 @@ class GasLogsController < ProtectedController
                                       :fuel,
                                       :brand,
                                       :price,
-                                      :total,
                                       :user_id)
     end
 end
--- app/models/gas_log.rb
+++ app/models/gas_log.rb
@@ -1,4 +1,4 @@
 class GasLog < ApplicationRecord
   belongs_to :user
-  validates :date, :odometer, :volume, :fuel, :brand, :price, :total, :user_id, presence: true
+  validates :date, :odometer, :volume, :fuel, :brand, :price, :user_id, presence: true
 end
